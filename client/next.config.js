4d490101eaa679dd86cdc287d3929dc8f59c9039
Andrey Bashun <a.a.bashun@gmail.com>
2023-03-29 22:15:51 +0300

    port config

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
+
+  env: {
+    SERVER_HOST: 'http://158.160.24.26:5000/',
+    SERVER_PORT: '5000',
+    DB_NAME: 'mrb-info',
+}
 }
 
 module.exports = nextConfig
